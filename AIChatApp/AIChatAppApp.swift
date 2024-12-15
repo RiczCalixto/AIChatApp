57eb8e27f04bddc3c1a18ce9435c6794778ac9e6
Ricardo Calixto <38529592+RiczCalixto@users.noreply.github.com>
2024-12-15 11:15:12 -0300

    Initial Commit

---
+++ AIChatApp/AIChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatAppApp.swift
+//  AIChatApp
+//
+//  Created by Taqtile on 15/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AIChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatAppTests/AIChatAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatAppTests.swift
+//  AIChatAppTests
+//
+//  Created by Taqtile on 15/12/24.
+//
+
+import Testing
+@testable import AIChatApp
+
+struct AIChatAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
